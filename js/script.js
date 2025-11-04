e7e8f784dfe73570490e1d1e21fd3555d325b449
eber28 <eberromanpumachahua@gmail.com>
2025-11-04 10:14:27 -0500

    feat:trabajo de hoy

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
-console.log("hola soy tareA ")
+const input=document.querySelector('input[type="text"]')
+const userInput=document.querySelector('#inputUsuario')
+const lista=document.querySelector("#lista")
+let idCounter=0
+const stats=document.querySelector("#stats")
+
+userInput.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    agregaTarea()
+})
+
+let agregaTarea=()=>{
+    idCounter++
+    let newValue=input.value
+    if (newValue==""){
+        alert("no puede tener una tarea vacia")
+        return
+    }
+    lista.innerHTML+=`
+        <div class="contenedor-tarea" id="${idCounter}">
+            <label>
+                <input type="checkbox">
+                ${newValue}
+            </label>
+            <img src="./assets/delete.png" alt="eliminar" class="btnEliminar">
+        </div>
+    `
+    input.value=""
+    updateStats()
+}
+
+lista.addEventListener('click', (event)=>{
+    if (event.srcElement.nodeName=="IMG"){
+        deleteTask(event.srcElement.parentNode.id)
+    }else if(event.srcElement.nodeName=="INPUT"){
+        updateStats()
+    }
+})
+
+let updateStats=()=>{
+    let divs=lista.querySelectorAll('div')
+    let check=lista.querySelectorAll('input[type="checkbox"]:checked')
+    stats.innerHTML=`
+        Tareas: ${divs.length}   Completadas: ${check.length}
+    `
+}
+
+let deleteTask=(id)=>{
+    let taskDelete=document.getElementById(id)
+    lista.removeChild(taskDelete)
+    updateStats()
+}
